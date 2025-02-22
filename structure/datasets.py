4788cacabcb5f67abd9de554e7286c794b7fc021
Eimy-san <152164314+Eimy-san@users.noreply.github.com>
2025-02-22 20:38:29 +0800

    Update datasets.py

--- structure/datasets.py
+++ structure/datasets.py
@@ -15,15 +15,13 @@ from torchvision.transforms.functional import adjust_gamma
 PARAMS = Params()
 
 def gamma_correction(image, gamma=2.2):
-    """应用伽马校正"""
     return adjust_gamma(image, gamma=gamma)
 
-# 弹性变换参数示例，根据需要调整
 elastic_transform_params = {
-    'alpha': 50,  # 控制扭曲的程度
-    'sigma': 30,  # 控制网格密度
-    'alpha_affine': 70,  # 控制仿射变换的程度
-    'p': 0.35,  # 设置p=1.0以确保每次调用都应用变换，便于观察
+    'alpha': 50, 
+    'sigma': 30,
+    'alpha_affine': 70, 
+    'p': 0.35, 
     'border_mode': 2,
 }
 
@@ -33,33 +31,22 @@ paddy_labels = {'Cholangiocarcinoma':0, 'HCC':1, 'Normal_Liver':2}
 
 class PaddyDataSet(Dataset):
     def __init__(self, data_dir, transform=None):
-        """
-        数据集
-
-        """
         self.label_name = {'Cholangiocarcinoma':0, 'HCC':1, 'Normal_Liver':2}
         self.transform = transform
         self.albumentation_transform = ElasticTransform(**elastic_transform_params)
-
-        # data_info 存储所有图片路径和标签, 在DataLoader中通过index读取样本
         self.data_info = self.get_img_info(data_dir)
-
-
+        
     def __getitem__(self, index):
         path_img, label = self.data_info[index]
         img = Image.open(path_img).convert('RGB')
-
-
         img = ImageOps.equalize(img)
         img = gamma_correction(img, gamma=2.2)
         if self.transform == train_transform:
             img_np = np.array(img)
             augmented = self.albumentation_transform(image=img_np)
             img = Image.fromarray(augmented['image'])
-
         if self.transform:
             img = self.transform(img)
-
         return img, label
 
     def __len__(self):
@@ -69,19 +56,15 @@ class PaddyDataSet(Dataset):
     def get_img_info(data_dir):
         data_info = list()
         for root, dirs, _ in os.walk(data_dir):
-            # 遍历类别
             for sub_dir in dirs:
                 img_names = os.listdir(os.path.join(root, sub_dir))
                 img_names = list(filter(lambda x: x.endswith('.jpg'), img_names))
-
-                # 遍历图片
                 for i in range(len(img_names)):
                     img_name = img_names[i]
                     path_img = os.path.join(root, sub_dir, img_name)
                     # print(sub_dir)
                     label = paddy_labels[sub_dir]
                     data_info.append((path_img, int(label)))
-
         return data_info
 
 
@@ -91,7 +74,7 @@ train_transform = transforms.Compose([
     transforms.RandomVerticalFlip(p=0.2),
     transforms.RandomResizedCrop(size=224, scale=(0.7,1.0),),
     # transforms.RandomRotation(10),
-    # elastic_transform,  # 添加弹性变换
+    # elastic_transform, 
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5002, 0.5002, 0.5002], std=[0.2874, 0.2874, 0.2874]),
 ])
